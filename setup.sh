e92d05fa6bc97db2dbbd388bb99b2c1870834561
Anuj Kumar Gupta <anujkr2631@gmail.com>
2025-08-15 01:18:13 +0530

    Updated the Site

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+mkdir -p ./data/uploads
+
+if [ ! -e ./env ]; then
+  echo "* Creating virtual env"
+  python -m venv env
+fi
+
+env/bin/activate.bat
+pip install torch --extra-index-url https://download.pytorch.org/whl/cpu
+
+pip install -r ./backend/requirements.txt
+
+if [ ! -e ./data/config.toml ]; then
+  echo "* Copying config file to data/config.toml"
+  cp ./sample/config-sample.toml ./data/config.toml
+
+  echo
+  echo "  *** Make sure to edit the following values in the config ***"
+  echo -e "  - auth_key: Your secret key if you plan to use Centio with a public IP"
+  echo -e "  - openai_base_url\n  - context_size (should be ~ 2/3 of actual token max)"
+  echo -e "  - openai_model (if not local)\n  - openai_api_key (if not local)"
+fi
+
